4d0694cf12989b959275515327e05c62b50e9f24
Matthew Nitschke <matthewcnitschke@gmail.comi>
2019-03-01 13:14:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# No-Ip dyn dns renewer
+A docker container to renew no-ip dyn dns records automatically
+
+
+# Running Locally
+```
+docker build -t no-ip-renewer:latest . && docker run no-ip-renewer:latest
+```
---
+++ app.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.noip.com/login?ref_url=console');
+
+    await page.type('input[name=username]', '')
+    await page.type('input[name=password]', '')
+    await page.$eval('#clogs', form => form.submit());
+
+    await page.screenshot({ path: 'example.png' });
+
+    await browser.close();
+})();
